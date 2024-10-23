de1da12a072580cbf536ae0df563a8669b0cf237
colince Tcheussieu Mendji <131427317+colincelemec@users.noreply.github.com>
2024-10-23 14:04:37 +0200

    Add files via upload

---
+++ Projet_example/AzertyType/Scripts/config.js
@@ -0,0 +1,5 @@
+
+//liste contenant les mots et les phrases
+
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = ["la maison de colince", "le chat et la souris", "le corbeau et le renard"];
